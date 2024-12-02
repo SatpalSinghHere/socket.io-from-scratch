b2d8d49a2d461c9039652aaf1565390d34257432
SatpalSinghHere <satpalsinghalive1@gmail.com>
2024-12-02 12:17:44 +0530

    "Updated index.html, index.js, and package.json files"

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from 'express'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 
 const PORT = 8000
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
 })
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+console.log(dirname(fileURLToPath(import.meta.url)))
